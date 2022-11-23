9492b9e25053cad06a247f8f7abf45fd5d96af6b
Kenichi Takahama <hammer528@msn.com>
2022-11-24 07:28:30 +0900

    Initial Commit

---
+++ Project5_part2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project5_part2
+//
+//  Created by K.Takahama on R 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project5_part2/Project5_part2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project5_part2App.swift
+//  Project5_part2
+//
+//  Created by K.Takahama on R 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project5_part2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
